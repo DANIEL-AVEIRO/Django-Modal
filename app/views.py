f6cd7edd551d1f03154121ab76b10b0c54b1487a
Daniel <codedbydaniel@gmail.com>
2025-11-12 13:35:54 +0630

    first

---
+++ app/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from app.models import ProductModel
+
+# Create your views here.
+
+
+def index(request):
+    products = ProductModel.objects.all().order_by("-created_at")
+    context = {
+        "products": products,
+    }
+    return render(request, "index.html", context)
+
+
+def product_create(request):
+    if request.method == "POST":
+        product = ProductModel.objects.create(
+            name=request.POST.get("name"),
+            price=request.POST.get("price"),
+        )
+        product.save()
+        return redirect("/")
+
+
+def product_update(request, pk):
+    if request.method == "POST":
+        product = get_object_or_404(ProductModel, id=pk)
+        product.name = request.POST.get("name")
+        product.price = request.POST.get("price")
+        product.save()
+        return redirect("/")
+
+
+def product_delete(request, pk):
+    product = get_object_or_404(ProductModel, id=pk)
+    product.delete()
+    return redirect("/")
---
+++ modal/urls.py
@@ -0,0 +1,30 @@
+"""
+URL configuration for modal project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from app import views
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", views.index),
+    path("product/create/", views.product_create, name="product_create"),
+    path("product/update/<int:pk>/", views.product_update, name="product_update"),
+    path("product/delete/<int:pk>/", views.product_delete, name="product_delete"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
